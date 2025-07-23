f76981742e5098c9edb233b2bc555ff12e692b81
NishithaSunka <sainishithasunka@gmail.com>
2025-07-23 21:59:30 +0530

    fix: Correct app initialization order in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
 const allowedOrigins = [
     'http://localhost:5173',         
     'https://fintrack-client-2wyu.onrender.com'
@@ -17,10 +23,8 @@ const corsOptions = {
     }
 };
 app.use(cors(corsOptions));
-dotenv.config();
-connectDB();
 
-const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
