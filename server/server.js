09ef7394071d58d10bb0fc787f5b67de661d783c
loksaikotini <132242927+loksaikotini@users.noreply.github.com>
2025-07-24 17:47:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-// server/server.js
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-
 dotenv.config();
 connectDB();
 
 const app = express();
+
 const allowedOrigins = [
-    'http://localhost:5173',         
+    'http://localhost:5173',
+
     'https://fintrack-expense-tracker.onrender.com'
 ];
+
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
@@ -24,14 +26,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
-app.use(cors());
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/transactions', require('./routes/transactions'));
 app.use('/api/budget', require('./routes/budget'));
 app.use('/api/groups', require('./routes/groups'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
