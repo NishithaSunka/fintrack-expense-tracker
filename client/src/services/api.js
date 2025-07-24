44966498ade4c37b04c517d78f1f7978f7c50942
loksaikotini <132242927+loksaikotini@users.noreply.github.com>
2025-07-24 17:46:28 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 
 import axios from 'axios';
+
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://fintrack-expense-tracker.onrender.com' 
+
+  ? 'https://fintrack-server-c70y.onrender.com/api' 
   : 'http://localhost:5000/api';
 
 const API = axios.create({
